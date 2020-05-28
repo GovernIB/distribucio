1928cf44d5388ec4933502cdeeea3924e20423fc
Josep Gayà <josepg@limit.es>
2020-05-28 09:31:39 +0200

    #158 Optimitzar el temps que arriba l’anotació fins a la bústia
    Correcció script per oracle
    
    Task-url: https://github.com/GovernIB/distribucio/issues/158

--- scripts/bbdd/1.0/oracle/updates/distribucio_update_0.9.27.sql
+++ scripts/bbdd/1.0/oracle/updates/distribucio_update_0.9.27.sql
@@ -1,4 +1,10 @@
--- #143 Error DIS_CONTINGUT_MULT_UK creant bústies
--- Elimina la restricció de la taula de continguts
+-- #157 Distribucio es queda sense connexions a BBDD 
+-- S'ha de millorar la consulta de registres. Es crea una columna nova per determinar si està pendent
 
-ALTER TABLE DIS_CONTINGUT DROP CONSTRAINT DIS_CONTINGUT_MULT_UK;
+-- Afegeix una columna de processament
+ALTER TABLE DISTRIBUCIO.DIS_REGISTRE ADD PENDENT NUMBER DEFAULT 1 NOT NULL;
+-- Posa com a processades les que tinguin l'estat de processat
+UPDATE DIS_REGISTRE SET PENDENT = 0
+WHERE PROCES_ESTAT IN ('BUSTIA_PROCESSADA', 'BACK_PENDENT', 'BACK_REBUDA', 'BACK_PROCESSADA', 'BACK_REBUTJADA', 'BACK_ERROR');
+-- Crea un índex per a la nova columna 
+CREATE INDEX DIS_REGISTRE_PENDENT_I ON DISTRIBUCIO.DIS_REGISTRE (PENDENT);
