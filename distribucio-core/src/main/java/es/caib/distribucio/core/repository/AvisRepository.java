b01c56b5b40633d37f8cacd938d20409dfe7a5c0
Josep Gayà <josepg@limit.es>
2020-08-14 15:19:52 +0200

    #215 Error consulta bústies actives a PostgreSQL i errors scripts
    Task-url: https://github.com/GovernIB/distribucio/issues/215

--- distribucio-core/src/main/java/es/caib/distribucio/core/repository/AvisRepository.java
+++ distribucio-core/src/main/java/es/caib/distribucio/core/repository/AvisRepository.java
@@ -11,28 +11,17 @@ import org.springframework.data.jpa.repository.Query;
 import es.caib.distribucio.core.entity.AvisEntity;
 
 /**
- * Definició dels mètodes necessaris per a gestionar una entitat de base
- * de dades del tipus bústia.
+ * Repositori per gestionar una entitat de base de dades del tipus avís.
  * 
  * @author Limit Tecnologies <limit@limit.es>
  */
 public interface AvisRepository extends JpaRepository<AvisEntity, Long> {
-
 
 	@Query(	"from " +
 			"    AvisEntity a " +
 			"where " +
 			"    a.actiu = true " +
 			"and a.dataInici<=CURRENT_DATE " +
-			"and a.dataFinal+1>CURRENT_DATE")
-	List<AvisEntity> findActive();
-	
-
-	@Query(	"select a.dataFinal, a.dataFinal+1, CURRENT_DATE from " +
-			"    AvisEntity a ")
-	List<Object> findDate();
-	
-
-	
-
+			"and a.dataFinal>=trunc(CURRENT_DATE)")
+	List<AvisEntity> findActive();	
 }
