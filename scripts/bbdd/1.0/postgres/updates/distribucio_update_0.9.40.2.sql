9c7f4212ecc02a8c58fcf6704c1d87f7546e7645
josepg-limit <josepg@limit.es>
2022-03-09 09:30:36 +0100

    Tancament DISTRIBUCIÓ v0.9.40.2 (2022.03.09 09:00h)
    - Nova propietat de número de threads per la descàrrega .zip afegida

---
+++ scripts/bbdd/1.0/postgres/updates/distribucio_update_0.9.40.2.sql
@@ -0,0 +1,2 @@
+-- #428 Revisar el rendiment en la descàrrega del ZIP del justificant i els annexos
+Insert into DIS_CONFIG (KEY,VALUE,DESCRIPTION,GROUP_CODE,POSITION,JBOSS_PROPERTY,TYPE_CODE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE) values ('es.caib.distribucio.contingut.generacio.zip.num.threads',null,'Número de threads màxim per a la consulta y generació del .zip de documentació (per defecte 3)','GENERAL','14','0','INT',null,null);
