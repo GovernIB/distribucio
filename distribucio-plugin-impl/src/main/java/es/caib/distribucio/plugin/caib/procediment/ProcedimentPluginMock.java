e1ab91b2d58c54776f2551f039de3e16a0d4ef9d
josepg-limit <josepg@limit.es>
2022-05-03 13:33:01 +0200

    #439 Revisar que accio classificar aparegui en historic accions

---
+++ distribucio-plugin-impl/src/main/java/es/caib/distribucio/plugin/caib/procediment/ProcedimentPluginMock.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package es.caib.distribucio.plugin.caib.procediment;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+
+import es.caib.distribucio.plugin.SistemaExternException;
+import es.caib.distribucio.plugin.procediment.Procediment;
+import es.caib.distribucio.plugin.procediment.ProcedimentPlugin;
+import es.caib.distribucio.plugin.utils.PropertiesHelper;
+
+/**
+ * Implementació del plugin de consulta de procediments emprant MOCK.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+public class ProcedimentPluginMock implements ProcedimentPlugin {
+
+	@Override
+	public List<Procediment> findAmbCodiDir3(
+			String codiDir3) throws SistemaExternException {
+		List<Procediment> response = new ArrayList<>();
+		Procediment p = new Procediment();
+		p.setCodigo("1324");
+		p.setCodigoSIA("1315");
+		response.add(p);
+		return response;
+	}
+
+}
