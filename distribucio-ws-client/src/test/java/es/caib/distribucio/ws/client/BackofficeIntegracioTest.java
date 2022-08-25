30e65a60612e7a64bf269713adc84b7f58ff7c98
josepg-limit <josepg@limit.es>
2022-08-25 13:47:09 +0200

    Configuracions test Backoffice

--- distribucio-ws-client/src/test/java/es/caib/distribucio/ws/client/BackofficeIntegracioTest.java
+++ distribucio-ws-client/src/test/java/es/caib/distribucio/ws/client/BackofficeIntegracioTest.java
@@ -22,8 +22,8 @@ import es.caib.distribucio.ws.backofficeintegracio.Estat;
  */
 public class BackofficeIntegracioTest {
 	// autofirma wih PADES
-	private static final String IDENTIFICADOR = "L11E1646737371605/2022"; //L11E1557929165976/2019
-	private static final String CLAU_ACCESS = "iqV8SiGk90e9sv2DqQfdCC0E4r//5M/WyG0K0fE19dg="; //jqCJ/24gm+j5VGtETYj8itrQmlD6zwHp4BL8g33ulh4=
+	private static final String IDENTIFICADOR = "GOIBE4347/2022"; //"L11E1646737371605/2022"; //DES: "GOIBE4347/2022" 
+	private static final String CLAU_ACCESS = "dwWOGZgn0kTPVex5QNdsymYR8MwFv01arTjJIOodgQQ="; //"iqV8SiGk90e9sv2DqQfdCC0E4r//5M/WyG0K0fE19dg="; //DES: "dwWOGZgn0kTPVex5QNdsymYR8MwFv01arTjJIOodgQQ="
 	
 //	// autofirma wih CADES
 //	private static final String IDENTIFICADOR = "L11E1557470209883/2019";
@@ -77,10 +77,25 @@ public class BackofficeIntegracioTest {
 
 	private BackofficeIntegracio getBustiaServicePort() throws IOException {
 		return BackofficeIntegracioWsClientFactory.getWsClient(
-				"http://10.35.3.232:8080/distribucio/ws/backofficeIntegracio",
-				"admin",				// proves amb usuari només a SEYCON
-				"admin");				// usuari: provesFernando
-	}									// contrasenya: provesFernando
+				"https://dev.caib.es/distribucio/ws/v1/bustia",
+				"u145691",				
+				"jhksFo54");				
+	}									
+	
+
+	/* DES */
+//	"http://10.35.3.232:8080/distribucio/ws/backofficeIntegracio",
+//	"admin",				
+//	"admin"
+	
+	/* DEV */
+//	"https://dev.caib.es/distribucio/ws/v1/bustia",
+//	"u145691",				
+//	"jhksFo54"
+	
+	/* PROVES AMB USUARI NOMÉS A SEYCON */
+//	usuari: provesFernando
+//	contrasenya: provesFernando
 	
 
 	
