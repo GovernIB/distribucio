68fa81fc7565d9ed99f4859715c0f6d1ac10623d
josepg-limit <josepg@limit.es>
2022-12-30 10:21:17 +0100

    Problema amb anotacions que es queden en estat "Comunicat al backoffice"

--- distribucio-api-interna/src/main/java/es/caib/distribucio/api/interna/controller/BackofficeRestController.java
+++ distribucio-api-interna/src/main/java/es/caib/distribucio/api/interna/controller/BackofficeRestController.java
@@ -2,6 +2,7 @@ package es.caib.distribucio.api.interna.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,10 @@ import es.caib.distribucio.core.api.exception.SistemaExternException;
 import es.caib.distribucio.core.api.service.ws.backoffice.AnotacioRegistreEntrada;
 import es.caib.distribucio.core.api.service.ws.backoffice.AnotacioRegistreId;
 import es.caib.distribucio.core.api.service.ws.backoffice.BackofficeIntegracioWsService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 /**
  * Controlador pel servei REST de backoffice.
@@ -26,6 +31,7 @@ import es.caib.distribucio.core.api.service.ws.backoffice.BackofficeIntegracioWs
  */
 @Controller
 @RequestMapping("/backoffice")
+@Slf4j
 public class BackofficeRestController {
 
     @Autowired
@@ -34,14 +40,29 @@ public class BackofficeRestController {
     @RequestMapping(
             value= "/consulta",
             method = RequestMethod.GET,
-            produces = "application/json")
+            produces = "application/json;charset=utf-8")
     @ApiOperation(
             value = "Consulta d'una anotació de registre pendent d'enviar al Backoffice",
             notes = "Retorna totes les dades de l'anotació de registre pendents d'enviar al Backoffice consultada")
+    @ResponseBody
     public ResponseEntity<AnotacioRegistreEntrada> consulta(
             HttpServletRequest request,
-            @ApiParam(name="id", value="Identificador de la anotació de registre")
-            final AnotacioRegistreId id) throws SistemaExternException {
+            @ApiParam(name="allParams", value="Mapa que conté tots els paràmetres. Ha d'incloure 2 paràmetres: 'indetificador' (identificador de l'anotació) i 'clauAcces' (clau proporcionada per a poder accedit a l'anotació)")
+            @RequestParam Map<String,String> allParams) {
+//            @ApiParam(name="id", value="Identificador de la anotació de registre")
+//            final AnotacioRegistreId id) throws SistemaExternException {
+        log.info("[REST CONSULTA] Paràmetres rebuts (" + allParams.size() + "):");
+        for (Map.Entry<String, String> entry: allParams.entrySet()) {
+            log.info("[REST CONSULTA]   - " + entry.getKey() + ": " + entry.getValue());
+        }
+
+        String identificador = allParams.get("indetificador");
+        String clauAcces = allParams.get("clauAcces");
+
+        AnotacioRegistreId id = new AnotacioRegistreId();
+        id.setIndetificador(identificador);
+        id.setClauAcces(clauAcces);
+
         AnotacioRegistreEntrada respuesta = backofficeIntegracioWsService.consulta(id);
         return new ResponseEntity<>(respuesta, HttpStatus.OK);
     }
@@ -49,10 +70,11 @@ public class BackofficeRestController {
     @RequestMapping(
             value= "/canviEstat",
             method = RequestMethod.POST,
-            produces = "application/json")
+            produces = "application/json;charset=utf-8")
     @ApiOperation(
             value = "Canvi d'estat d'una anotació de registre",
             notes = "Canvia l'estat d'una anotació de registre enviada a backoffice")
+    @ResponseBody
     public ResponseEntity<Void> canviEstat(
             HttpServletRequest request,
             @ApiParam(name="infoCanviEstat", value="Informació del canvi d'estat. Inclou l'identificador de l'anotació de registrem, l'estat que es vol assignar a l'anotació, i observacions.")
