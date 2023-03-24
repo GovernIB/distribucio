c884811e9908e2df089bb8537507248239fea6de
Josep Gayà <josepg@limit.es>
2023-03-24 10:51:34 +0100

    #576 Revisar les propietats fixades null no s'actualitzen. Correcció sql

--- versions/0.9.48/576.sql
+++ versions/0.9.48/576.sql
@@ -1,5 +1,6 @@
 #576     Revisar per què les propietats fixades a null no s'actualitzen
 
+ 
 UPDATE DIS_CONFIG  t
-   SET t."KEY"  = REPLACE(t."KEY" , 'csv.definicio', 'csv_generation_definition')
-  WHERE t."KEY" LIKE  'es.caib.distribucio%.plugin.arxiu.caib.csv.definicio';
+  SET t."KEY"  = REPLACE(t."KEY" , 'csv_generation_definition', 'csv.generation.definition')
+  WHERE t."KEY" LIKE  'es.caib.distribucio%.plugin.arxiu.caib.csv_generation_definition';
