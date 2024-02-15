8a4047728d9c6a89426639b71f4b1fa6cf70d98f
Josep Gayà <josepg@limit.es>
2024-02-15 09:46:13 +0100

    #655 Canvis en les regles
    - Posar per propietat avaluar totes les regles.

--- versions/9.0.50.2/655.sql
+++ versions/9.0.50.2/655.sql
@@ -1,12 +1,18 @@
 --#655 Dues regles distintes amb mateix SIA i amb UO i bústia d'origen definides, s'executen al mateix temps
 
--- Nova columna per configurar les regles per finalitzar avaluació
 
 -- Oracle
 
+-- Nova columna per configurar les regles per finalitzar avaluació
 ALTER TABLE DIS_REGLA ADD ATURAR_AVALUACIO NUMBER(1) DEFAULT 0;
+-- Nova propietat per establir si avaluar totes les regles o només la primera
+Insert into DIS_CONFIG (KEY,VALUE,DESCRIPTION,GROUP_CODE,POSITION,JBOSS_PROPERTY,TYPE_CODE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE) values ('es.caib.distribucio.tasca.aplicar.regles.avaluar.totes',null,'Avaluar totes les regles en comtes de trobar la primera','SCHEDULLED_REGLES','2','0','BOOL',null,null);
+
 
 -- Postgresql
 
+-- Nova columna per configurar les regles per finalitzar avaluació
 ALTER TABLE DIS_REGLA ADD COLUMN ATURAR_AVALUACIO BOOLEAN;
+-- Nova propietat per establir si avaluar totes les regles o només la primera
+Insert into DIS_CONFIG (KEY,VALUE,DESCRIPTION,GROUP_CODE,POSITION,JBOSS_PROPERTY,TYPE_CODE,LASTMODIFIEDBY_CODI,LASTMODIFIEDDATE) values ('es.caib.distribucio.tasca.aplicar.regles.avaluar.totes',null,'Nombre de reintents de aplicar regles','SCHEDULLED_REGLES','2',false,'BOOL',null,null);
 
