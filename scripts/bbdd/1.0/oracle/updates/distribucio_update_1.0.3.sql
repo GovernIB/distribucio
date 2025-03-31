89674701e1b60ace95140cc6c45f83d66b382cac
Josep Gayà <josepg@limit.es>
2025-03-31 18:59:20 +0200

    Correccions scripts SQL Distribucio 1.0.3

--- scripts/bbdd/1.0/oracle/updates/distribucio_update_1.0.3.sql
+++ scripts/bbdd/1.0/oracle/updates/distribucio_update_1.0.3.sql
@@ -19,6 +19,9 @@ CREATE TABLE DIS_SERVEI (
  ALTER TABLE DIS_SERVEI ADD (
   CONSTRAINT DIS_SERVEI_PK PRIMARY KEY (ID));
 
+-- Assigna permisos a la taula
+GRANT SELECT, UPDATE, INSERT, DELETE ON DIS_SERVEI TO WWW_DISTRIBUCIO;
+
 -- dis_procediment foreign keys
 
 ALTER TABLE dis_servei ADD CONSTRAINT dis_servei_entitat_fk FOREIGN KEY (entitat) REFERENCES dis_entitat(id);
@@ -67,11 +70,11 @@ INSERT INTO DIS_CONFIG
 ("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
 VALUES('es.caib.distribucio.plugin.rolsac.service.url', 'https://dev.caib.es/rolsac/api/rest/v1', 'Url per a accedir a Rolsac', 'PROCEDIMENTS', 1, 1, 'TEXT', NULL, 1, NULL, NULL);
 
-INSERT INTO DIS_CONFIG
-("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
-VALUES('es.caib.distribucio.tasca.monitor.integracio.actualitzar.serveis', '0 0 17 * * *', 'Especificar l''expressió ''cron'' indicant l''interval de temps de les actualitzacions dels serveis en segon pla. Per defecte s''aplicarà el cron corresponent per actualitzar cada divendres a les 15:30h', 'SCHEDULLED_PROCEDIMENT', 7, 0, 'TEXT', NULL, 0, NULL, TIMESTAMP '2025-01-09 15:39:49.120000');
-
 INSERT INTO DIS_CONFIG_GROUP
 (CODE, PARENT_CODE, "POSITION", DESCRIPTION)
 VALUES('SCHEDULLED_SERVEI', 'SCHEDULLED', 23, 'Tasca periòdica per actualitzar la taula de serveis');
+
+INSERT INTO DIS_CONFIG
+("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
+VALUES('es.caib.distribucio.tasca.monitor.integracio.actualitzar.serveis', '0 0 17 * * *', 'Especificar l''expressió ''cron'' indicant l''interval de temps de les actualitzacions dels serveis en segon pla. Per defecte s''aplicarà el cron corresponent per actualitzar cada divendres a les 15:30h', 'SCHEDULLED_SERVEI', 0, 0, 'TEXT', NULL, 0, NULL, TIMESTAMP '2025-01-09 15:39:49.120000');
 
--- versions/1.0.3/720.sql
+++ versions/1.0.3/720.sql
@@ -4,10 +4,10 @@ INSERT INTO DIS_CONFIG
 ("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
 VALUES('es.caib.distribucio.plugin.rolsac.service.url', 'https://dev.caib.es/rolsac/api/rest/v1', 'Url per a accedir a Rolsac', 'PROCEDIMENTS', 1, 1, 'TEXT', NULL, 1, NULL, NULL);
 
-INSERT INTO DIS_CONFIG
-("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
-VALUES('es.caib.distribucio.tasca.monitor.integracio.actualitzar.serveis', '0 0 17 * * *', 'Especificar l''expressió ''cron'' indicant l''interval de temps de les actualitzacions dels serveis en segon pla. Per defecte s''aplicarà el cron corresponent per actualitzar cada divendres a les 15:30h', 'SCHEDULLED_PROCEDIMENT', 7, 0, 'TEXT', NULL, 0, NULL, TIMESTAMP '2025-01-09 15:39:49.120000');
-
 INSERT INTO DIS_CONFIG_GROUP
 (CODE, PARENT_CODE, "POSITION", DESCRIPTION)
 VALUES('SCHEDULLED_SERVEI', 'SCHEDULLED', 23, 'Tasca periòdica per actualitzar la taula de serveis');
+
+INSERT INTO DIS_CONFIG
+("KEY", VALUE, DESCRIPTION, GROUP_CODE, "POSITION", JBOSS_PROPERTY, TYPE_CODE, LASTMODIFIEDBY_CODI, CONFIGURABLE, ENTITAT_CODI, LASTMODIFIEDDATE)
+VALUES('es.caib.distribucio.tasca.monitor.integracio.actualitzar.serveis', '0 0 17 * * *', 'Especificar l''expressió ''cron'' indicant l''interval de temps de les actualitzacions dels serveis en segon pla. Per defecte s''aplicarà el cron corresponent per actualitzar cada divendres a les 15:30h', 'SCHEDULLED_SERVEI', 0, 0, 'TEXT', NULL, 0, NULL, TIMESTAMP '2025-01-09 15:39:49.120000');
