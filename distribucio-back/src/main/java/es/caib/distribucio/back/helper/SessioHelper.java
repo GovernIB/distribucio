76cf2f6bd78a6b88a3e2f298b50e4361918f9993
josepg-limit <josepg@limit.es>
2025-10-01 09:23:38 +0200

    Permetre escollir l'entitat per defecte #713

--- distribucio-back/src/main/java/es/caib/distribucio/back/helper/SessioHelper.java
+++ distribucio-back/src/main/java/es/caib/distribucio/back/helper/SessioHelper.java
@@ -6,8 +6,8 @@ package es.caib.distribucio.back.helper;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import es.caib.distribucio.logic.helper.ConfigHelper;
 import es.caib.distribucio.logic.intf.dto.EntitatDto;
+import es.caib.distribucio.logic.intf.service.ConfigService;
 import es.caib.distribucio.logic.intf.service.EntitatService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,7 +38,8 @@ public class SessioHelper {
 			HttpServletRequest request,
 			HttpServletResponse response,
 			AplicacioService aplicacioService,
-            EntitatService entitatService) {
+            EntitatService entitatService,
+            ConfigService configService) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		if (auth != null) {
 			Boolean autenticacioProcessada = (Boolean)request.getSession().getAttribute(
@@ -55,7 +56,7 @@ public class SessioHelper {
 			}
 			try {
                 EntitatDto entitatActual = EntitatHelper.getEntitatActual(request, entitatService);
-                ConfigHelper.setEntitat(entitatActual);
+                configService.setEntitatPerPropietat(entitatActual);
 				String idioma_usuari = aplicacioService.getUsuariActual().getIdioma();
 				request.getSession().setAttribute(
 						SESSION_ATTRIBUTE_IDIOMA_USUARI,
--- distribucio-back/src/main/java/es/caib/distribucio/back/interceptor/SessioInterceptor.java
+++ distribucio-back/src/main/java/es/caib/distribucio/back/interceptor/SessioInterceptor.java
@@ -6,6 +6,7 @@ package es.caib.distribucio.back.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import es.caib.distribucio.logic.intf.service.ConfigService;
 import es.caib.distribucio.logic.intf.service.EntitatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,13 +26,14 @@ public class SessioInterceptor implements AsyncHandlerInterceptor {
 	@Autowired
 	private AplicacioService aplicacioService;
     @Autowired private EntitatService entitatService;
+    @Autowired private ConfigService configService;
 
 	@Override
 	public boolean preHandle(
 			HttpServletRequest request,
 			HttpServletResponse response,
 			Object handler) throws Exception {
-        SessioHelper.processarAutenticacio(request, response, aplicacioService, entitatService);
+        SessioHelper.processarAutenticacio(request, response, aplicacioService, entitatService, configService);
 		return true;
 	}
 
