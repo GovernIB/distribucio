cd5525de7dd5ea2cf97b644c54d80bbb5a71a88a
josepg-limit <josepg@limit.es>
2025-12-18 14:40:35 +0100

    Crear API REST per a que Comanda pugui consultar els logs de l'aplicació #824

--- distribucio-service-intf/src/main/java/es/caib/distribucio/logic/intf/service/EstadisticaService.java
+++ distribucio-service-intf/src/main/java/es/caib/distribucio/logic/intf/service/EstadisticaService.java
@@ -6,17 +6,24 @@ import java.util.List;
 import es.caib.comanda.model.v1.estadistica.DimensioDesc;
 import es.caib.comanda.model.v1.estadistica.IndicadorDesc;
 import es.caib.comanda.model.v1.estadistica.RegistresEstadistics;
+import es.caib.distribucio.logic.intf.config.BaseConfig;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface EstadisticaService {
 
-	public List<DimensioDesc> getDimensions();
+	@PreAuthorize("hasRole('DIS_COM')")
+    public List<DimensioDesc> getDimensions();
 
-	public List<IndicadorDesc> getIndicadors();
+	@PreAuthorize("hasRole('DIS_COM')")
+    public List<IndicadorDesc> getIndicadors();
 
-	public RegistresEstadistics consultaUltimesEstadistiques();
+	@PreAuthorize("hasRole('DIS_COM')")
+    public RegistresEstadistics consultaUltimesEstadistiques();
 
-	public RegistresEstadistics consultaEstadistiques(LocalDate localData);
+	@PreAuthorize("hasRole('DIS_COM')")
+    public RegistresEstadistics consultaEstadistiques(LocalDate localData);
 
-	public List<RegistresEstadistics> consultaEstadistiques(LocalDate startLocalData, LocalDate endLocalData);
+	@PreAuthorize("hasRole('DIS_COM')")
+    public List<RegistresEstadistics> consultaEstadistiques(LocalDate startLocalData, LocalDate endLocalData);
 
 }
