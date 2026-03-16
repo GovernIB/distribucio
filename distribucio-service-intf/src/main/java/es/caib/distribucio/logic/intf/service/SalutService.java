2502c3de50e4aaf3f3a1366ae34c5aaf701761b0
josepg-limit <josepg@limit.es>
2026-03-16 10:08:24 +0100

    Afegir autenticació a la consulta de salut per comanda #839

--- distribucio-service-intf/src/main/java/es/caib/distribucio/logic/intf/service/SalutService.java
+++ distribucio-service-intf/src/main/java/es/caib/distribucio/logic/intf/service/SalutService.java
@@ -6,13 +6,18 @@ import es.caib.comanda.model.v1.salut.ContextInfo;
 import es.caib.comanda.model.v1.salut.IntegracioInfo;
 import es.caib.comanda.model.v1.salut.SalutInfo;
 import es.caib.comanda.model.v1.salut.SubsistemaInfo;
+import es.caib.distribucio.logic.intf.config.BaseConfig;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface SalutService {
 
+    @PreAuthorize("hasRole('" + BaseConfig.ROLE_COMANDA + "')")
 	List<IntegracioInfo> getIntegracions();
 
+    @PreAuthorize("hasRole('" + BaseConfig.ROLE_COMANDA + "')")
 	List<SubsistemaInfo> getSubsistemes();
-	
+
+    @PreAuthorize("hasRole('" + BaseConfig.ROLE_COMANDA + "')")
 	public List<ContextInfo> getContexts(String baseUrl);
 
 	SalutInfo checkSalut(String version, String string);
